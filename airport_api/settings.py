c5ccc069fd3b6ed79992ec3b6af9e8598bed0421
Dmytro Shulha <dimonchik0218@gmail.com>
2026-08-13 20:10:42 +0300

    add settings & rest_framework, delete template

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "airport",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = "airport_api.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
+        "DIRS": []
         ,
         "APP_DIRS": True,
         "OPTIONS": {
