c852666a76921b93529ebc445f0242c8e10b3557
Dmytro Shulha <dimonchik0218@gmail.com>
2026-08-15 20:21:32 +0300

    add routers to all viewsets

---
+++ airport/urls.py
@@ -0,0 +1,33 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from airport.views import (
+    CountryViewSet,
+    CityViewSet,
+    AirportViewSet,
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    RouteViewSet,
+    CrewViewSet,
+    FlightViewSet,
+    OrderViewSet,
+    TicketViewSet,
+)
+
+app_name = "airport"
+
+router = routers.DefaultRouter()
+router.register("countries", CountryViewSet)
+router.register("cities", CityViewSet)
+router.register("airports", AirportViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("routes", RouteViewSet)
+router.register("crews", CrewViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
+router.register("tickets", TicketViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
