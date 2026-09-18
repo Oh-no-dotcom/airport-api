d1599e836a5f553cc8ef1926d7186cafb0d625b6
Dmytro Shulha <dimonchik0218@gmail.com>
2026-09-18 11:50:50 +0300

    delete empty file tests.py & add DEBUG, SECRET_KEY to .env

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-iid6upz&7vu=o-fn@vds-%s)lx(+9+(faxkm(t7kzvc48)q-4h"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
